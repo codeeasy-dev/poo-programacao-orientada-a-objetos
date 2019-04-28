cf8a27f7d744e252bcdba102b758a5cb148c7f62
Felipe Vieira <feliperenanvieira@gmail.com>
2019-04-28 11:06:20 -0300

    Aula 06 - 1º Pilar - Encapsulamento

---
+++ aula_06/php/Person.php
@@ -0,0 +1,29 @@
+<?php
+
+class Person
+{
+    /** @var string $name */
+    private $name;
+    /** @var int $age */
+    private $age;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+        return $this;
+    }
+}
---
+++ aula_06/php/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/Person.php';
+
+$felipe = new Person();
+$felipe->setName("Felipe Renan Vieira")
+    ->setAge(18);
